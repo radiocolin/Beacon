f52daffb78b22de2ac91f983209c83e345e361d3
Colin Weir <colin@radioboy.us>
2026-03-18 14:11:35 -0400

    Initial Commit

---
+++ Beacon/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Beacon
+//
+//  Created by Colin Weir on 3/18/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
